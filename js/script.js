1a2e9a0dd32b10e67d2f0dccd3bfe59fe85b94b9
Evandro Barros <evandrobarrosb98@gmail.com>
2023-05-30 18:50:31 -0300

    Incluiado alternar cor parado e andamento, style e script

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let hour = document.querySelector("#hour");
 let minute = document.querySelector("#minute");
 let segund = document.querySelector("#segund");
 
+let tempo = document.querySelectorAll(".tempo div p");
+
 let iniciar = document.querySelector("#start");
 let pausar = document.querySelector("#stop");
 let zerar = document.querySelector("#zerar");
@@ -34,10 +36,12 @@ let timer;
                 houra++;
                 hour.innerHTML = zeroAEsquerda(houra);
             }
-        },1000)        
+        },1000)
+        mudarCorTempo(false);        
     })
     pausar.addEventListener("click",()=>{
         clearInterval(timer);
+        mudarCorTempo(true);
     })
     zerar.addEventListener("click",()=>{
         houra,minutos,segundos = 0;
@@ -45,11 +49,10 @@ let timer;
         minute.innerHTML = "00";
         segund.innerHTML = "00";
         clearInterval(timer)
-
-        console.log(houra,minutos,segundos);
     })
 
-
-
-
-
+    let mudarCorTempo = (valida) => {
+        tempo.forEach((elementos)=> {
+            valida ? elementos.setAttribute("class","tempo-parado") : elementos.classList.remove("tempo-parado");
+        })
+    }
